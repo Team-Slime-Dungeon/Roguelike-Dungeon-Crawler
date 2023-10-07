cae9961748a50f1e609b02d30d831b06a6aba3c5
Leondra <quietxof@protonmail.com>
2023-10-07 13:32:17 -0700

    Kobi's dungeon code implemented

--- room_generation.gd
+++ room_generation.gd
@@ -1,38 +1,50 @@
 extends Node2D
 
 var random = RandomNumberGenerator.new()
-var floorstruct = 0 #dictionary, room size and child info
+var node_pos = [] 
+var num_paths = 10 #the number of hallway segments
+var segment_length = 10 #length of hallway segments
 
 func _ready():
-	floorstruct = {0: [0, 0]} #empty main room
-	clear_room() #removes tiles based on largest room size (400 tile radius)
-	create_rooms() #generate main room and children rooms
-	$debug_seed_info.set_text("Debug\n " + str(floorstruct)) #debug info visual
+	clear_room()
+	generate_hallways() #first, generate the hallways
+	generate_room() #second, create rooms on top of selected nodes
+	$debug_seed_info.set_text("Debug\n " + str(node_pos)) #debug info visual
 	
-func create_rooms():
-	floorstruct[0][0] = getsize() #get size of root room
-	floorstruct[0][1] = getchild() #get child room number
-	
-	for i in floorstruct[0][1]: #get size of room for all rooms generated
-		floorstruct[i+11] = [getsize(), 0]
-	
-	for key in floorstruct: #build rooms on canvas
-		build_room(key, (floorstruct[key][0]))
-	return floorstruct
-
-func getsize(): return random.randi_range(3, 5)
-func getchild(): return random.randi_range(4, 4)
 func _on_new_seed_pressed(): return _ready() #click button for new build
-func build_room(key, size):
-	for i in range(-size, size):
-		for j in range(-size, size):
-			if key == 0:$TileMap.set_cell(0, Vector2i(i, j), 0, Vector2i(0, 0))
-			elif key == 11:$TileMap.set_cell(0, Vector2i(i+(size*3), j+(size)), 0, Vector2i(0, 0))
-			elif key == 12:$TileMap.set_cell(0, Vector2i(i+(size), j-(size*3)), 0, Vector2i(0, 0))
-			elif key == 13:$TileMap.set_cell(0, Vector2i(i-(size*3), j-(size)), 0, Vector2i(0, 0))
-			elif key == 14:$TileMap.set_cell(0, Vector2i(i-(size), j+(size*3)), 0, Vector2i(0, 0))
+
+func generate_hallways():
+	var direction = Vector2i(1, 0) #tracks which way the hall segment will travel
+	var position = Vector2i(0, 0) #tracks current tile position
+	node_pos.append(position) #initial room at 0,0
+	
+	# this loop makes hallway segments
+	for j in range(0, num_paths):
+		for i in range(0, segment_length): # this loop creates 1 segment
+			position += direction
+			$TileMap.set_cell(0, position, 0, Vector2i(0, 0))
+		if !position in node_pos: node_pos.append(position) # since can overlap, avoid duplicates in list
+		direction = get_new_direction(direction) # get a new direction for the next segment
+
+# this function determines the direction of the new hallway segment 
+# restriction: new direction should never be the reverse of the previous direction
+func get_new_direction(old_direction):
+	var directions = [Vector2i(0,1), Vector2i(0, -1), Vector2i(1, 0), Vector2i(-1, 0)]
+	var result = -old_direction
+	while result == -old_direction:
+		result = directions[random.randi_range(0, 3)]
+	return result
+
+func generate_room():
+	for i in node_pos:
+		var size = random.randi_range(1, 2)
+		for j in range(-size, size+1):
+			for k in range(-size, size+1):
+				$TileMap.set_cell(0, Vector2i(i.x+j, i.y+k), 
+					0, Vector2i(0,0))	
 
 func clear_room():
-	for i in range(-200, 200):
+	node_pos = [] 
+	for i in range(-1000, 1000):
 		for j in range(-200, 200):
 			$TileMap.erase_cell(0, Vector2i(i , j))
