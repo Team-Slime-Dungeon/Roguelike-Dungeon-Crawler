dec34e82ebea319439d7c33d142f85b6d2abb0e0
Leondra <quietxof@protonmail.com>
2023-10-15 15:15:23 -0700

    minor sprite fixes

---
+++ Cassandra.gd
@@ -0,0 +1,68 @@
+extends Skeleton2D
+
+var move_speed = 100  # Adjust the movement speed as needed
+@onready var anim = get_node("AnimationPlayer")
+func _process(delta):
+	# Initialize movement direction
+	var move_direction = Vector2(0, 0)
+	
+	
+	
+	# Check for user input (arrow keys)
+	
+	if Input.is_action_pressed("ui_left"):
+		move_direction.x -= 1
+		
+		#checks if there is double input and displays appropriate animation
+		if Input.is_action_pressed('ui_up') and Input.is_action_pressed("ui_left"):
+			anim.play("walking_left_up")
+		
+		else:
+			anim.play("walking_left")
+			
+	if Input.is_action_pressed("ui_right"):
+		move_direction.x += 1
+		
+		#checks if there is double input and displays appropriate animation
+		if Input.is_action_pressed('ui_up') and Input.is_action_pressed("ui_right"):
+			anim.play("walking_right_up")
+		else:
+			anim.play("walking_right")
+			
+	if Input.is_action_pressed("ui_up"):
+		move_direction.y -= 1
+		
+		#checks if there is double input and checks which two inputs they are and displays appropriate animation
+		if Input.is_action_pressed('ui_up') and Input.is_action_pressed("ui_left"):
+			anim.play("walking_left_up")
+		elif Input.is_action_pressed('ui_up') and Input.is_action_pressed("ui_right"):
+			anim.play("walking_right_up")
+		else:
+			anim.play("walking_left")
+			
+	if Input.is_action_pressed("ui_down"):
+		move_direction.y += 1
+		
+		#checks if there is double input and displays appropriate animation
+		if Input.is_action_pressed("ui_left") && Input.is_action_pressed("ui_down"):
+			anim.play("walking_left")
+		else:
+			anim.play("walking_right")
+		
+	
+	#animation stops if there is no input	
+	if move_direction.length() == 0:
+		anim.stop()
+		
+	
+	
+	# Normalize the movement direction (optional)
+	if move_direction.length() > 0:
+		move_direction = move_direction.normalized()
+		
+
+	# Update the position of the skeleton
+	var new_position = position + move_direction * move_speed * delta
+	set_position(new_position)
+
+	
--- debug_UI.gd
+++ debug_UI.gd
@@ -3,6 +3,8 @@ extends CanvasLayer
 @export var max_zoom: float = 3.0
 @export var min_zoom: float = 0.5
 @export var drag_speed: float = 200.0
+@export var shake_duration: float = 0.5 # Duration of the screen shake
+@export var shake_intensity: float = 5.0 #Intensitiy of the screen shake
 
 @onready var cam = get_node("../Cassandra/Camera2D")
 @onready var CAS = get_node("../Cassandra")
@@ -11,6 +13,22 @@ var zoom_max  = Vector2(1.0,1.0)# max zoom
 var zoom_speed1 = Vector2(.3000001,.3000001)# speed of zooming
 var velocity =  Vector2();
 
+var is_shaking = false
+var shake_timer = 0.0
+
+func _process(delta): # This function is called every frame to handle screen shaking.
+	if is_shaking: 
+		shake_timer += delta #modifies the camera's position
+		if shake_timer >= shake_duration:
+			is_shaking = false
+			shake_timer = 0.0
+			#cam.position = CAS.position #Returns to player Cam
+			cam.position = cam.position #Returns to Debug Cam
+		else:
+				var random_offset = Vector2(randf() * 2 - 1, randf() * 2 - 1) * shake_intensity
+				cam.position = cam.position + random_offset #Returns to Debug Cam
+				# cam.position = CAS.position + random_offset  #Returns to Player Cam
+				
 func _on_in_zoom_pressed():
 	#if cam.zoom < zoom_m:
 	cam.zoom += zoom_speed1
@@ -39,3 +57,7 @@ func _on_up_pressed():
 func _on_down_pressed():
 	velocity.y += 100.0
 	cam.position = velocity
+
+func _on_shake_pressed():
+	is_shaking = true
+	#print("Debug")
