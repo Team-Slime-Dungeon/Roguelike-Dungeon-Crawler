fba50976d3cd444d07bd6aa0a0641ecfb0cb0faa
Leondra <quietxof@protonmail.com>
2023-10-22 13:02:23 -0700

    trying to resolve conflict

--- Cassandra.gd
+++ Cassandra.gd
@@ -2,64 +2,67 @@ extends Skeleton2D
 
 var move_speed = 100  # Adjust the movement speed as needed
 @onready var anim = get_node("AnimationPlayer")
+
+# Add a variable to track the player's last movement direction
+var last_direction = Vector2(1, 0)  # Initialize it to face right
+
 func _process(delta):
-	# Initialize movement direction
 	var move_direction = Vector2(0, 0)
-	
-	
-	
+
 	# Check for user input (arrow keys)
-	
 	if Input.is_action_pressed("ui_left"):
 		move_direction.x -= 1
-		
-		#checks if there is double input and displays appropriate animation
-		if Input.is_action_pressed('ui_up') and Input.is_action_pressed("ui_left"):
+		last_direction = Vector2(-1, 0)  # Update the last_direction when moving left
+		# Check for double input and display appropriate animation
+		if Input.is_action_pressed('ui_up'):
 			anim.play("walking_left_up")
-		
 		else:
 			anim.play("walking_left")
-			
-	if Input.is_action_pressed("ui_right"):
-		move_direction.x += 1
 		
-		#checks if there is double input and displays appropriate animation
-		if Input.is_action_pressed('ui_up') and Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("ui_right"):
+		move_direction.x += 1
+		last_direction = Vector2(1, 0)  # Update the last_direction when moving right
+		# Check for double input and display appropriate animation
+		if Input.is_action_pressed('ui_up'):
 			anim.play("walking_right_up")
 		else:
 			anim.play("walking_right")
-			
+		
+	else:
+		# No horizontal movement input, play the appropriate idle animation
+		if last_direction.x < 0:
+			anim.play("idle_left")
+		elif last_direction.x > 0:
+			anim.play("idle_right")
+
 	if Input.is_action_pressed("ui_up"):
 		move_direction.y -= 1
-		
-		#checks if there is double input and checks which two inputs they are and displays appropriate animation
-		if Input.is_action_pressed('ui_up') and Input.is_action_pressed("ui_left"):
+		# Check for double input and display appropriate animation
+		if last_direction.x < 0:
 			anim.play("walking_left_up")
-		elif Input.is_action_pressed('ui_up') and Input.is_action_pressed("ui_right"):
+		elif last_direction.x > 0:
 			anim.play("walking_right_up")
 		else:
 			anim.play("walking_left")
 			
 	if Input.is_action_pressed("ui_down"):
 		move_direction.y += 1
-		
-		#checks if there is double input and displays appropriate animation
-		if Input.is_action_pressed("ui_left") && Input.is_action_pressed("ui_down"):
+		# Check for double input and display appropriate animation
+		if last_direction.x < 0:
 			anim.play("walking_left")
-		else:
+		elif last_direction.x > 0:
 			anim.play("walking_right")
-		
 	
-	#animation stops if there is no input	
+	# Animation stops if there is no input
 	if move_direction.length() == 0:
-		anim.play("idle")
-		
-	
-	
+		if last_direction.x < 0:
+			anim.play("idle_left")
+		elif last_direction.x > 0:
+			anim.play("idle_right")
+
 	# Normalize the movement direction (optional)
 	if move_direction.length() > 0:
 		move_direction = move_direction.normalized()
-		
 
 	# Update the position of the skeleton
 	var new_position = position + move_direction * move_speed * delta
