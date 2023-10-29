4ab0c1d1ec60420bba14c4d064b29fc1e36bd61f
JesseCarrilloCS <jesse.carrillo.876@my.csun.edu>
2023-10-29 06:03:52 -0700

    Bugfix attempt on Dungeon generation (get_perpendicular_vector, get_new_direction and the wall generation for paths.) This should prevent hallway walls from spawning over hallway areas.

--- room_generation.gd
+++ room_generation.gd
@@ -4,13 +4,13 @@ extends Node2D
 var random = RandomNumberGenerator.new()
 var node_pos = []
 var hallway_pos = []
-var num_paths = 10 #the number of hallway segments
+var num_paths = 55 #the number of hallway segments
 var segment_length = 10 #length of hallway segments
-var room_min = 2 #smallest size for rooms
+var room_min = 3 #smallest size for rooms
 var room_max = 4 #largest size for rooms
 var zoom_int = 1 #debug zoom
-var min_path_width = 4 # 2 Walls + Path
-var max_path_width = 5
+var min_path_width = 2 # Minimum path size
+var max_path_width = 3 # Max path size
 
 func _ready():
 	heartsContainer.setMaxHearts(player.maxHealth)
@@ -31,21 +31,32 @@ func generate_hallways():
 	# this loop makes hallway segments
 	for j in range(0, num_paths):
 		var row_width = random.randi_range(min_path_width, max_path_width)
+		var perpendicular = get_perpendicular_vector(direction)
+
 		for i in range(0, segment_length): # this loop creates 1 segment
+
 			position += direction
 			# create a row of path width
-			var perpendicular = get_perpendicular_vector(direction)
 			var row_position = position - floor(row_width / 2) * perpendicular
-			# Loop for making individual tiles in the bridge
+			
+			# Create the outside wall of path if possible
+			if Vector2i(row_position) not in hallway_pos:
+				$TileMap.set_cell(0, Vector2i(row_position.x-1, row_position.y-1), 1, Vector2i(3, 1))
+			else:
+				print("Wall/ Hallway Conflict Wall 1: ", row_position)
+			
+			# Create a row of hallway and save it in a list
 			for k in range(0, row_width):
-				# Draw the walls of the walkway
-				if (k == 0) or k == (row_width-1):
-					$TileMap.set_cell(0, row_position, 1, Vector2i(0, 0))
-				else:
-				# The actual walkway, saved for use later to prevent walls overwriting them.
-					hallway_pos.append(Vector2i(row_position)) # Used in wall generation
-					$TileMap.set_cell(0, row_position, 0, Vector2i(0, 3))
+				$TileMap.set_cell(0, row_position, 0, Vector2i(0, 3))
+				hallway_pos.append(Vector2i(row_position.x, row_position.y))
 				row_position += perpendicular
+	
+			# Create second hallway wall if possible			
+			if Vector2i(row_position) not in hallway_pos:
+				$TileMap.set_cell(0, Vector2i(row_position.x, row_position.y), 1, Vector2i(3, 1))
+			else:
+				print("Wall/ Hallway Conflict Wall 2: ", row_position)	
+
 		if !position in node_pos: node_pos.append(position) # since can overlap, avoid duplicates in list
 		direction = get_new_direction(direction) # get a new direction for the next segment
 
@@ -53,27 +64,52 @@ func generate_hallways():
 # restriction: new direction should never be the reverse of the previous direction
 func get_new_direction(old_direction):
 	var directions = [Vector2i(0,1), Vector2i(0, -1), Vector2i(1, 0), Vector2i(-1, 0)]
-	directions.erase(old_direction)
-	return directions[random.randi_range(0, directions.size() - 1)]
+	#print("=======")
+	var reverse
+	
+	# Remove the reverse of the old direction
+	if old_direction == Vector2i(1,0):
+		reverse = Vector2i(-1,0)
+		#print("Moved Up!")
+	elif old_direction == Vector2i(-1,0):
+		reverse = Vector2i(1,0)
+		#print("Moved Down")
+	elif old_direction == Vector2i(0,1):
+		reverse = Vector2i(0,-1)
+		#print("Moved Right")
+	elif old_direction == Vector2i(0,-1):
+		reverse = Vector2i(0,1)
+		#print("Moved Left")
+	
+	directions.erase(reverse)
+	var new_directions = directions[random.randi_range(0, directions.size() - 1)]
+	#print("Old Directions: ", old_direction, " New directions: ", new_directions)
+	return new_directions
 
 # this function returns a vector that is perpendicular
 # to the passed in vector2i
 func get_perpendicular_vector(original):
-	return Vector2i(-original.y, original.x)
+	var perpen
+	if original == Vector2i(1,0) or original == Vector2i(0,-1):
+		perpen = Vector2i(-original.y, original.x)
+	else:
+		perpen = Vector2i(original.y,-original.x)
+	return perpen
 
 func generate_room():
 	var cells = []
 	
 	for i in node_pos:
 		var size = random.randi_range(room_min, room_max)
-		for j in range(-size-1, size+1):
-			for k in range(-size-1, size+1):
+
+		for j in range(-size, size):
+			for k in range(-size, size):
 				# Walls furthest from camera
-				if ((j == -size-1)) or ((k == size)) or ((j == size) or (k == -size-1)):
+				if ((j == -size)) or ((k == size-1)):
 					if !(Vector2i(i.x+j,i.y+k)) in hallway_pos: 
 						$TileMap.set_cell(0, Vector2i(i.x+j, i.y+k), 1, Vector2i(0, 0))
 				# Walls close to camera	
-				elif ((j == size) or (k == -size-1)):
+				elif ((j == size-1) or (k == -size)):
 					if !(Vector2i(i.x+j,i.y+k)) in hallway_pos:
 						$TileMap.set_cell(0, Vector2i(i.x+j, i.y+k), 1, Vector2i(0, 0))
 				# Stores tile location to be connected
@@ -86,6 +122,7 @@ func generate_room():
 func clear_room():
 	node_pos = []
 	hallway_pos = []
-	for i in range(-1000, 1000):
-		for j in range(-1000, 1000):
+	for i in range(-100, 100):
+		for j in range(-100, 100):
 			$TileMap.erase_cell(0, Vector2i(i , j))
+			$TileMap.set_cell(0, Vector2i(i,j), 1, Vector2i(2, 0))
