50b879348045234ab6d33071bb83e0a2e32780dc
KillerSV13 <javier.galangarcia.997@my.csun.edu>
2024-03-06 22:07:36 -0800

    removed bug with weapon textures

--- InventoryTesting/Playerequip_inv/EquipSlot.gd
+++ InventoryTesting/Playerequip_inv/EquipSlot.gd
@@ -43,22 +43,46 @@ func _can_drop_data(at_position, data):
 	return !is_occupied and data.has("origin_texture")
 
 func _drop_data(at_position, data):
-	if _can_drop_data(at_position, data):
-		texture = data["origin_texture"]
-		is_occupied = true
-		
-		# Clear the origin slot if it's different from the target slot
-		if data["origin_slot"] != self:
-			data["origin_slot"].clear_slot()
+	var origin_slot = data["origin_slot"]
+	var item_id = data["item_id"] # 
+	var item_name = data["item_name"]
+	data["item_id"] = item_id
+	var item_count = data["item_count"] 
+	var amount = item_count
+	var new_weapon_id = data["item_name"]
 	
-	#gets the the icon's textures
-	#gets the weapon's id by it associated texture
-	#equips the weapon (adds it the equip weapon stats list) and deletes the weapon from the main inventory
-	#prints out the current weapon
-	#prints out the new inventory
+	if item_name: # Check if `item_name` is not empty
+		#Items.Player_Inventory.get_item_name(item_id)
+		emit_signal("item_equipped", item_name)
+		self.set_meta("equipped_item_name", item_name) # Set metadata
+		var texture_path = "res://InventoryTesting/Item Test/%s.png" % item_name
+		var loaded_texture = load(texture_path)
+		if loaded_texture:
+			self.texture = loaded_texture
+			is_occupied = true # Update the slot's occupation
+			
+			
+	print("Item equipped: ", item_name, " with ID ", item_id)
+	
+	#Items.Player_Inventory.equip_weapon(new_weapon_id)
+	
+	#if _can_drop_data(at_position, data):
+		#texture = data["origin_texture"]
+		#is_occupied = true
+		#
+		## Clear the origin slot if it's different from the target slot
+		#if data["origin_slot"] != self:
+			#data["origin_slot"].clear_slot()
+	#
+	##gets the the icon's textures
+	##gets the weapon's id by it associated texture
+	##equips the weapon (adds it the equip weapon stats list) and deletes the weapon from the main inventory
+	##prints out the current weapon
+	##prints out the new inventory
 	var item_texture = icon.get_texture()
 	var equip_weapon_id = Items.Player_Inventory.get_item_id_by_texture(item_texture)
-	Items.Player_Inventory.equip_weapon(equip_weapon_id)	
+	Items.Player_Inventory.equip_weapon(equip_weapon_id)
+	icon.set_meta("equipped_item_id", equip_weapon_id)
 	var current_weapon = Items.Player_Inventory.get_current_weapon()
 	print("The current weapon is ", current_weapon)
 	Items.Player_Inventory._print_inventory()
---
+++ InventoryTesting/unequipbutton.gd
@@ -0,0 +1,45 @@
+extends TextureButton
+var is_occupied = false
+@onready var icon = $"../Tabs/Inventory/EquipBackground/MainHand/Icon"
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+#func _get_weapon_texture():
+	#return icon.get_texture()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.aw
+func _process(delta):
+	pass
+	
+func _can_drop_data(at_position, data):
+	return !is_occupied and data.has("origin_texture")
+	var origin_slot = data["origin_slot"]
+	
+	return self == origin_slot or !is_occupied
+func _drop_data(at_position, data):
+	
+	var item_texture = data ["origin_texture"]
+	var item_id = Items.Player_Inventory.get_item_id_by_texture(item_texture)
+	
+	var origin_slot = data["origin_slot"]
+	var target_slot = self # Since this function is on the slot, 'self' refers to the target slot
+	# Check if dropping back to the origin slot or moving to a new slot
+	#if icon.has_meta("equipped_item_id"):
+		#var item_id = icon.get_meta("equipped_item_id")
+		#Items.Player_Inventory._delete_equip(item_id)
+	if data["origin_texture"] != self:
+		data["origin_slot"].clear_slot()
+		Items.Player_Inventory._delete_equip(item_id)
+		
+			
+			#deletes the weapon from the equipped dictionary
+			
+	
+	
+
+func _on_mouse_entered():
+	modulate = Color(0.8, 0.8, 0.8) # Gray shade
+
+
+func _on_mouse_exited():
+	modulate = Color(1, 1, 1) # Normal color
