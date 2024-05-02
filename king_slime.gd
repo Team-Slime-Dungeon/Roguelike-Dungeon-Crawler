35e529e700602011d9856154a0ab9916aa3badef
JesseCarrilloCS <jesse.carrillo.876@my.csun.edu>
2024-05-02 12:47:06 -0700

    Fixed score screen crash, added some boss fight checks for animations, restricted Slime King hit animations and gave him a little bit of a invincibility window after being hit.

--- king_slime.gd
+++ king_slime.gd
@@ -10,6 +10,7 @@ var player_chase = false
 var currentHealth: int = 20
 var max_speed = 20
 var death_location = null
+var enemy_state = "alive"
 
 #Debug Step back in case it has crashes it can be turned off here by making it false
 var allow_step_back = true
@@ -34,29 +35,27 @@ func _ready():
 func _physics_process(delta):	
 	if player_chase and is_instance_valid(player):
 		velocity = (player.position + Vector2(16,16) - self.position) + velocity / chase_speed
+	
 	else: 
-		move_timer += delta
-	
-		if move_timer >= 1.0:  # Adjust this value to control the time for each type of movement
-			move_timer = 0
-			current_state = get_direction()
-	
-		match current_state:
-			MovementState.UP: velocity.y -= speed * delta
-			MovementState.LEFT: velocity.x -= speed * delta
-			MovementState.DOWN: velocity.y += speed * delta
-			MovementState.RIGHT: velocity.x += speed * delta
-	
-		if velocity.x >= max_speed:
-			velocity.x = max_speed
-			
-		if velocity.y >= max_speed:
-			velocity.y = max_speed
-
-		#position = delta * motion
-		#motion = Vector2.ZERO 
-	
-	move_and_slide() # Need for collision
+		if enemy_state != "dead": # Stop movement on death
+			move_timer += delta
+		
+			if move_timer >= 1.0:  # Adjust this value to control the time for each type of movement
+				move_timer = 0
+				current_state = get_direction()
+		
+			match current_state:
+				MovementState.UP: velocity.y -= speed * delta
+				MovementState.LEFT: velocity.x -= speed * delta
+				MovementState.DOWN: velocity.y += speed * delta
+				MovementState.RIGHT: velocity.x += speed * delta
+		
+			if velocity.x >= max_speed:
+				velocity.x = max_speed
+				
+			if velocity.y >= max_speed:
+				velocity.y = max_speed
+		move_and_slide() # Need for collision
 	
 func get_direction():
 	var current_direction = random.randi_range(1, 5)  # Select direction
@@ -68,7 +67,6 @@ func get_direction():
 		5: return null
 
 func enemy_clear(): queue_free()
-
 
 func step_back():
   # Check if player exists and is valid
@@ -95,23 +93,36 @@ func step_back():
 		resume_chase_timer.start()
 
 func resume_chase(): player_chase = true 
-
+var animation_running = false
 func _on_hurtbox_area_entered(area):
 	if area.name == "weapon" or area.name == "Shuriken":
-		currentHealth -= 1
-		print("King Slime HP: ", currentHealth)
-		$AnimationPlayer.play("hit")
-		#slimehitsound.play()
-		if currentHealth > 0:
-			$Hurt.start()
-			await $Hurt.timeout
-			$AnimationPlayer.play("movement")
-		#if currentHealth <= 0:
+		if animation_running == false and currentHealth > 0 and enemy_state != "dead":
+			currentHealth -= 1
+			print("King Slime HP: ", currentHealth)
+
+			#slimehitsound.play()
+			# Handles the slime hit animation
+			if currentHealth > 0:
+				print("KS still alive")
+				animation_running = true
+				$AnimationPlayer.play("hit")
+				$Hurt.start()
+				await $Hurt.timeout
+				$AnimationPlayer.play("movement")
+				animation_running = false
+				print("Resuming KS animation...")
+			else:
+				print("Slime is dead")
+		if animation_running == true and currentHealth > 0 and enemy_state != "dead":
+			print("blocked animation interupt")
+	
+		if currentHealth <= 0:
 			#$Death.start()
 			#$AnimationPlayer.play("death")
 			#await $Death.timeout
 			#slimedeathsound.play()
 			death_location = get_position()
+			enemy_state = "dead"
 	# start at 20
 	# when health reduced to 15, split, start timer
 	# rejoin health = slimes left * 5
@@ -125,7 +136,8 @@ func _on_detectionarea_1_body_entered(body):
 	player = body
 # Wait for 0.2 seconds before chasing
 	await get_tree().create_timer(0.2).timeout
-	player_chase = true 
+	if enemy_state != "dead":
+		player_chase = true 
 
 func _on_detectionarea_1_body_exited(body):
 	player = null
