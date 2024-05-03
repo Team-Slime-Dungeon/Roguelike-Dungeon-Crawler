788a4056456dfc2b87f6ca5a43228819a4341688
rick00002 <ricardo.santana.786@my.csun.edu>
2024-05-02 19:12:37 -0700

    Colorblind Added to Main Menu

--- ColorblindOptionsMenu.gd
+++ ColorblindOptionsMenu.gd
@@ -10,8 +10,10 @@ var bldMode
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#_setLabel(SettingVal.colorMode)
-	_loadColBlind(SettingVal.colorMode)
+	
 	count = SettingVal.colorMode
+	bldMode = $"../../../ColorBlindCanvasMM/ColorRect"
+	_loadColBlind(count)
 	#bldMode = $"../../CanvasLayer/ColorRect"
 	#bldMode = get_node("../../../ColorBlind/ColorRectBld")
 	
@@ -26,8 +28,10 @@ func _setLabel(i):
 	
 func _Lbuttonspressed():
 	if count != 0:
+		$"../../../ColorBlindCanvasMM".show()
 		count = count-1
 		_setLabel(count)
+		bldMode.material.set("shader_parameter/mode", count)
 	else:
 		count = 0
 	SettingVal.colorMode= count
@@ -37,13 +41,18 @@ func _Rbuttonspressed():
 	if count < 2:
 		count = count+1
 		_setLabel(count)
+		bldMode.material.set("shader_parameter/mode", count)
 	else:
 		_setLabel(3)
 		count = 3
+		$"../../../ColorBlindCanvasMM".hide()
 	SettingVal.colorMode= count
 	print(count)
 	
 func _loadColBlind(val):
+	if(val >= 0 && val <=2):
+		$"../../../ColorBlindCanvasMM".show()
+	bldMode.material.set("shader_parameter/mode", val)
 	set_text(arr[val])
 	pass
 	
