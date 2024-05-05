81a66e8e4a52ab3c6380b49c3a1a5a30e54f43d6
JesseCarrilloCS <jesse.carrillo.876@my.csun.edu>
2024-05-05 05:39:28 -0700

    Changed slime behavior and spawning in boss fight

--- environment/King_Slime_Fight.gd
+++ environment/King_Slime_Fight.gd
@@ -7,7 +7,18 @@ var king_slime = null
 var death_location = null
 var midfight = false
 var end_fight = false
+
+var enemy_spawn_pads = []
+var vase_spawn_pads = []
+
 var monster_spawns = []
+var total_spawns = 0
+
+var vase_spawns = []
+var vase_totals = 0
+
+var vase_item_spawns = []
+var vase_item_spawn_ID = 0
 
 #var score = preload("res://score.tscn")
 # Called when the node enters the scene tree for the first time.
@@ -24,7 +35,32 @@ func _ready():
 		Boss_Colors[1],
 		Boss_Colors[0],
 	)
-	
+	## Fill spawn locations for the slimes
+	if fight_state == 0 and end_fight == false:
+		# Inside skull
+		for i in range(12,15):
+			for j in range(20,23):
+				enemy_spawn_pads.append(Vector2i(i,j))
+		# Top left side
+		for i in range(13,20):
+			enemy_spawn_pads.append(Vector2(i,0))
+		# Top right side
+		for i in range(13,21):
+			enemy_spawn_pads.append(Vector2(i,8))
+			
+		for i in range(-8,8):
+			for j in range(8,18):
+				enemy_spawn_pads.append(Vector2i(i,j))
+
+		# Spawn locations for the support items		
+		for i in range(-3,10):
+			vase_spawn_pads.append(Vector2i(i,-4))
+		
+		place_supports(5)
+		# Begin fight
+		fight_state = 1
+		
+			
 	if fight_state == 99 and end_fight == false:
 		$Boss_Scene/AnimationPlayer.play("death")
 		await $Boss_Scene/AnimationPlayer.animation_finished
@@ -41,16 +77,34 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	for chest in vase_spawns:
+		if is_instance_valid(chest) and chest.death_location != null:
+			generate_loot(chest)
+			chest.clear_chest()
+			
+	for spawneditem in vase_item_spawns:
+		if is_instance_valid(spawneditem) and spawneditem.picked_up != false:
+			Items.Player_Inventory._add_item(spawneditem.ID, spawneditem.amount)
+			Items.Player_Inventory._print_inventory()
+			spawneditem.clear_item()		
 	
 	# Spawns baby slimes
-	if $Boss_Scene.currentHealth <= 10 and midfight == false: 
+	#if $Boss_Scene.currentHealth <= 10 and midfight == false:
+	if $Boss_Scene.currentHealth <= 12 and midfight == false:  
 		midfight = true
-		for i in range(10):
-			var scene = preload("res://monsters/Slime.tscn")
-			var slime = scene.instantiate()
-			monster_spawns.append(slime)
-			add_child(slime)
-			monster_spawns[i] = $BossFloor.map_to_local(Vector2i(5+i, 20+i))
+		print("Spawning in minion wave 1")
+		spawn_slimes(3)
+		fight_state = 2
+
+	if $Boss_Scene.currentHealth <= 8 and fight_state == 2:
+		print("Spawning in minion wave 2")
+		spawn_slimes(3)
+		fight_state = 3
+	
+	if $Boss_Scene.currentHealth <= 4 and fight_state == 3:
+		print("Spawning in minion wave 3")
+		spawn_slimes(4)
+		fight_state = 4
 	
 	# Removes all slimes and monsters
 	for monster in monster_spawns:
@@ -62,3 +116,80 @@ func _process(delta):
 		fight_state = 99
 		_ready()
 
+func spawn_slimes(spawn_num):
+	for i in range(spawn_num):
+		var scene = preload("res://monsters/Slime.tscn")
+		var slime = scene.instantiate()
+		slime.set_detection_radius(4)
+		
+		#enemy_spawn_pads.shuffle()
+		#var spawn_spot = enemy_spawn_pads[0] / 2
+		var spawn_spot = enemy_spawn_pads[randi() % enemy_spawn_pads.size()]
+		enemy_spawn_pads.erase(spawn_spot)
+			
+		monster_spawns.append(slime)
+		add_child(slime)
+
+		monster_spawns[total_spawns].global_position = $BossDeco.map_to_local(spawn_spot) / 2
+		total_spawns += 1
+
+func place_supports(vase_num):
+	for i in range(vase_num):
+		var support_scene = preload("res://container.tscn")
+		var spawnedContainer = support_scene.instantiate()
+
+		var container_list = ["Crate","Chest","Vase"]
+		var container_type = container_list[randi() % container_list.size()]
+		spawnedContainer.set_container(container_type)
+		
+		var spawn_spot = vase_spawn_pads[randi() % vase_spawn_pads.size()]
+		
+		vase_spawn_pads.erase(spawn_spot)
+
+		vase_spawns.append(spawnedContainer)
+		add_child(spawnedContainer)
+			
+		vase_spawns[vase_totals].global_position = $BossDeco.map_to_local(spawn_spot) / 2
+		print("Spawned container at actual:", vase_spawns[vase_totals].global_position)
+		vase_totals += 1
+
+func generate_loot(monster):
+	var loot_table = [-1]
+	var enemy_loot = []
+	
+	# Adds monster specific drops to the loot table
+	if "monster_drops" in monster:
+		enemy_loot = monster.monster_drops
+	
+	# Dropped item text flair
+	var monster_type = "Container"
+	if "monster_type" in monster:
+		monster_type = monster.monster_type
+	
+	if enemy_loot != []:
+		loot_table = enemy_loot
+		
+	# Draw an item from the loot table at random
+	loot_table.shuffle()
+	var loot_ID = loot_table[0]
+	
+	if loot_ID == -1: print(monster_type, " was empty.") # No Drops
+	
+	# Check to ensure the drawn item ID exists in the item list
+	elif Items.Player_Inventory.get_item_name(loot_ID) != "None":
+		
+		# Display text that changes depending on the rarity of your drop
+		if loot_ID > 9: print( monster_type, " dropped a Rare ", Items.Player_Inventory.get_item_name(loot_ID),"!!") # Rare Drop Text
+		else: print(monster_type, " dropped a ", Items.Player_Inventory.get_item_name(loot_ID),".") # Common Item Text
+
+	var new_item = Items.Player_Inventory.Item_Scenes[loot_ID]
+	var new_item_spawn = new_item.instantiate()
+	
+	# Manages all the item spawns to remove them when the floor is cleared.
+	vase_item_spawns.append(new_item_spawn)
+	add_child(new_item_spawn)
+	var new_item_location = Vector2i(monster.death_location)
+	
+	vase_item_spawns[vase_item_spawn_ID].global_position = new_item_location * 2
+	print("Should match:", vase_item_spawns[vase_item_spawn_ID].global_position) #new_item_location
+	vase_item_spawn_ID += 1
